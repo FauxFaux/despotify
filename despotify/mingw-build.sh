3b9a7b9afa4879f434c96f62fbe7ecd128c05289
Chris West (Faux) <git@goeswhere.com>
2011-04-09 22:15:56 +0100

    libdespotify.a generated

--- despotify/mingw-build.sh
+++ despotify/mingw-build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-make CC='gcc -I'$(pwd)'/../openssl/include -I'$(pwd)'/../HOME/include -DEZXML_NOMMAP'
+make CC='gcc -I'$(pwd)'/../openssl/include -I'$(pwd)'/../HOME/include -DEZXML_NOMMAP' install SUBDIRS=lib LDCONFIG=true DESTDIR=$(pwd)/../HOME
