efb8e0956d964f614f98e8a49b2b219142f66e88
Chris West (Faux) <git@goeswhere.com>
2011-04-10 16:17:05 +0100

    Raaaaage NIH build system

--- despotify/mingw-build.sh
+++ despotify/mingw-build.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 set -eu
-make install \
-	CC='gcc -I'$(pwd)'/../openssl/include -I'$(pwd)'/../HOME/include -DEZXML_NOMMAP -shared' \
-	SUBDIRS=lib \
-	LDCONFIG=true \
-	INSTALL_PREFIX=$(pwd)/../HOME \
-	"$@"
+cd lib
+OUT=../../HOME/lib
+gcc -DDEBUG_LOGIN -DDEBUG_PACKETS -DDEBUG -DEZXML_NOMMAP -D_WIN32_WINNT=0x600 \
+	-D'nanosleep(x,y)=Sleep(100)' -std=gnu99 -I../../HOME/include/ *.c -c
+
+gcc libdespotify.def -shared -Wl,--out-implib,libdespotify.dll.a \
+	-o libdespotify.dll -B$OUT *.o \
+	-lws2_32 -lz -lpthread -ldnsapi -logg -lvorbisfile -lcrypto -lgdi32
+
+cp libdespotify.dll.a $OUT
+cp libdespotify.dll $OUT/../bin
+cp libdespotify.dll /c/code/foo_input_spotify/foobar2000/
+
